794e26d8c353179ee4dcdab44fd2c0b954a69cc8
Abdullah Fahad <abdullah84f@gmail.com>
2022-09-24 18:34:41 +0300

    add domain-selection code.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,29 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+// wrap element with span tag
+const items = document.querySelectorAll(".block-domain__prices>li");
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    reset();
+    setDomain(this);
+  });
+});
+
+function setDomain(item) {
+  const span = document.createElement("span");
+  span.classList.add("badge");
+  span.classList.add("badge--secondary");
+  span.textContent = item.textContent;
+  item.textContent = "";
+  item.appendChild(span);
+}
+
+function reset() {
+  const items = document.querySelectorAll(".block-domain__prices>li");
+  items.forEach((item) => {
+    const span = item.querySelector("span");
+    if (span) item.textContent = span.textContent;
+  });
+}
